22ce0d9e41c703f161de630f5345c6e6000d5ff3
javier2409 <javiferreyra97@gmail.com>
2019-06-09 18:41:18 -0300

    warning message if quick-saving, more code comments

--- addscript.lua
+++ addscript.lua
@@ -1,11 +1,23 @@
 function addScriptToMap(mapname)
+	if mapname then --mapname is defined only on full-save
+		g_MapName = mapname
+	end
 
-	map = Resource.getFromName(mapname)
-	if not map then
-		outputChatBox(string.format('There is no map with name: %s',mapname))
+	if (not g_MapName) then -- if never full-saved
+		outputChatBox('Could not add script to map, please do full-save first')
 		return
 	end
 
+
+	-- Just in case, map name should be defined at this point with a valid one
+	map = Resource.getFromName(g_MapName)
+	if not map then
+		outputChatBox(string.format('There is no map with name: %s',g_MapName))
+		return
+	end
+
+
+	-- acl permission check
 	local thisresourcename = getThisResource():getName()
 	local admingroup = ACLGroup.get('MapEditor')
 	local requiredobject = string.format('resource.%s',thisresourcename)
@@ -14,31 +26,38 @@ function addScriptToMap(mapname)
 		return
 	end
 
-
+	-- check autojump script exists
 	if not Resource.getFromName('autojump') then
 		outputChatBox('Resource name must be "autojump", do not change it!')
 		return
 	end
 
-	local destination = string.format(':%s/autojump.lua',mapname)
+	-- copy script to map
+	local destination = string.format(':%s/autojump.lua',g_MapName)
 	fileCopy(':autojump/client.lua',destination,true)
-	local meta = XML.load(string.format(':%s/meta.xml',mapname))
-	local editorMeta = XML.load(':editor_test/meta.xml')
 
+	-- get necessary xml files
+	local meta = XML.load(string.format(':%s/meta.xml',g_MapName))
+	local editorMeta = XML.load(':editor_test/meta.xml')
+	local mapFile = XML.load(string.format(':%s/%s',g_MapName,meta:findChild('map',0):getAttribute('src')))
+
+	-- if the script is already there, notify that it was updated and finish
 	metaNodes = meta:getChildren()
 	for i,node in ipairs(metaNodes) do
 		if node:getName() == 'script' then
 			if node:getAttribute('src') == 'autojump.lua' then
-				outputChatBox(string.format('Script updated successfully on %s',mapname))
+				outputChatBox(string.format('Script updated successfully on %s',g_MapName))
 				return 
 			end
 		end
 	end
 
+	-- add script to map meta
 	local script = meta:createChild('script')
 	script:setAttribute('src','autojump.lua')
 	script:setAttribute('type','client')
 
+	-- add OOP attribute to map and editor_test too
 	if not meta:findChild('oop',0) then
 		local oop = meta:createChild('oop')
 		oop:setValue('true')
@@ -54,6 +73,7 @@ function addScriptToMap(mapname)
 	editorMeta:unload()
 
 
-	outputChatBox(string.format('Sucessfully added autojump script to map: %s',mapname))
+	outputChatBox(string.format('Sucessfully added autojump script to map: %s',g_MapName))
 end
 addEventHandler('saveResource',root,addScriptToMap)
+addEventHandler('quickSaveResource',root,addScriptToMap)
