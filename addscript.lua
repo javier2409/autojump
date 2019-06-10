d90c058fac3baf687163d8f8babbe25cf9f03623
javier2409 <javiferreyra97@gmail.com>
2019-06-10 05:59:19 -0300

    script gets deleted from map if no autojumps present

--- addscript.lua
+++ addscript.lua
@@ -1,21 +1,27 @@
 successcolor = '#49d154'
 failurecolor = '#d14b49'
+attributes = {}
+attributes['autojumpstart'] = {'id','rot_help','speed','precision','duration','model','end','posX','posY','posZ','rotX','rotY','rotZ'}
+attributes['autojumpend'] = {'id','model','posX','posY','posZ','rotX','rotY','rotZ'}
 
 function updateMapName(map)
 	g_MapName = map:getName()
 end
 addEventHandler('onMapOpened',root,updateMapName)
 
-function addScriptToMap(mapname)
-	if mapname then --mapname is defined only on full-save
-		g_MapName = mapname
+function findNode (xmlFile,childName,srcName)
+	local metaNodes = xmlFile:getChildren()
+	for i,node in ipairs(metaNodes) do
+		if node:getName() == childName then
+			if node:getAttribute('src') == srcName then
+				return node
+			end
+		end
 	end
+	return false
+end
 
-	if (not g_MapName) then -- if never full-saved
-		outputChatBox(string.format('%sAutojump: Could not add script to map, please do full-save first',failurecolor),root,0,0,0,true)
-		return
-	end
-
+function addScriptToMap()
 
 	-- Just in case, map name should be defined at this point with a valid one
 	map = Resource.getFromName(g_MapName)
@@ -40,34 +46,57 @@ function addScriptToMap(mapname)
 		return
 	end
 
-	-- copy script to map
-	local destination = string.format(':%s/autojump.lua',g_MapName)
-	fileCopy(':autojump/client.lua',destination,true)
-
 	-- get necessary xml files
 	local meta = XML.load(string.format(':%s/meta.xml',g_MapName))
 	local editorMeta = XML.load(':editor_test/meta.xml')
 	local mapFile = XML.load(string.format(':%s/%s',g_MapName,meta:findChild('map',0):getAttribute('src')))
 
-	updated = true
-	-- if the script is already there, notify that it was updated and finish
-	local metaNodes = meta:getChildren()
-	for i,node in ipairs(metaNodes) do
-		if node:getName() == 'script' then
-			if node:getAttribute('src') == 'autojump.lua' then
-				outputChatBox(string.format('%sAutojump: Script updated successfully on %s',successcolor,g_MapName),root,0,0,0,true)
-
-				copyAutojumpElementsToMap(mapFile,g_MapName,meta)
-				--copyAutojumpElementsToMap(mapFile,'editor_test',editorMeta) This is map editor's job
-
-				editorMeta:saveFile()
-				editorMeta:unload()
-				mapFile:unload()
-				meta:saveFile()
-				meta:unload()
-				return 
-			end
+	-- if no autojump nodes remove script from map as it is not needed
+	if #getAutojumpNodesFromMap(mapFile) == 0 then
+		deleted = false
+		if fileExists(':'..g_MapName..'/autojump.lua') then
+			fileDelete(':'..g_MapName..'/autojump.lua')
+			deleted = true
 		end
+
+		if fileExists(':'..g_MapName..'/autojump_data.xml') then
+			fileDelete(':'..g_MapName..'/autojump_data.xml')
+			deleted = true
+		end
+		local node1 = findNode(meta,'script','autojump.lua')
+		local node2 = findNode(meta,'file','autojump_data.xml')
+		if node1 then node1:destroy() end
+		if node2 then node2:destroy() end
+		editorMeta:saveFile()
+		editorMeta:unload()
+		mapFile:unload()
+		meta:saveFile()
+		meta:unload()
+		if deleted then
+			outputChatBox(successcolor..'Autojump: Script removed from '..g_MapName..' (no autojumps present)', root,0,0,0,true)
+		end
+		return		
+	end
+
+	-- copy script to map
+	local destination = string.format(':%s/autojump.lua',g_MapName)
+	fileCopy(':autojump/client.lua',destination,true)
+
+	updated = true
+
+	-- if the script is already there, notify that it was updated and finish
+	if findNode(meta,'script','autojump.lua') then
+		outputChatBox(string.format('%sAutojump: Script updated successfully on %s',successcolor,g_MapName),root,0,0,0,true)
+
+		copyAutojumpElementsToMap(mapFile,g_MapName,meta)
+	--copyAutojumpElementsToMap(mapFile,'editor_test',editorMeta) This is map editor's job
+
+		editorMeta:saveFile()
+		editorMeta:unload()
+		mapFile:unload()
+		meta:saveFile()
+		meta:unload()
+		return
 	end
 
 	-- add script to map meta
@@ -100,37 +129,42 @@ end
 
 function filter_addScriptToMap (value)
 	if not updated then
-		outputChatBox(failurecolor..'Autojump: Autojumps in map are not updated, save map before testing.',root,0,0,0,true)
+		outputChatBox(failurecolor..'Autojump: Autojumps may not be updated, save map before testing.',root,0,0,0,true)
 	end
-	updated = false
 end
 addEventHandler('testResource',root,filter_addScriptToMap)
 
 function waitTime ()
+
+	if (not g_MapName) then -- if never full-saved
+		outputChatBox(string.format('%sAutojump: Could not add script to map, please do full-save first',failurecolor),root,0,0,0,true)
+		killTimer(waitTimer)
+		return
+	end
+
 	meta = XML.load(string.format(':%s/meta.xml',g_MapName))
 	b = meta:findChild('map',0)
 	meta:unload()	
 
+
 	if b then
-		addScriptToMap()
 		killTimer(waitTimer)
+		addScriptToMap()
 	end
 end
 
 function setWaitTime(mapName)
 	g_MapName = mapName
-	outputChatBox(successcolor..'Autojump: Waiting for map to be saved',root,0,0,0,true)
-	waitTimer = setTimer(waitTime,100,0)
+	--outputChatBox(successcolor..'Autojump: Waiting for map to be saved',root,0,0,0,true)
+	waitTimer = setTimer(waitTime,500,40)
+end
+function setWaitTime_quicksave()
+	waitTimer = setTimer(waitTime,500,40)
 end
 addEventHandler('saveResource',root,setWaitTime)
-addEventHandler('quickSaveResource',root,setWaitTime)
+addEventHandler('quickSaveResource',root,setWaitTime_quicksave)
 
 function copyAutojumpElementsToMap(mapFile,mapName,mapMeta)
-
-	local attributes = {}
-	attributes['autojumpstart'] = {'id','rot_help','speed','precision','duration','model','end','posX','posY','posZ','rotX','rotY','rotZ'}
-	attributes['autojumpend'] = {'id','model','posX','posY','posZ','rotX','rotY','rotZ'}
-
 
 	-- check if map's meta already has autojump_data.xml
 	local found = false
@@ -152,20 +186,7 @@ function copyAutojumpElementsToMap(mapFile,mapName,mapMeta)
 	end
 
 	-- gather autojump data from map
-	local autojumpNodes = {}
-
-	local mapNodes = mapFile:getChildren()
-	for i,node in ipairs(mapNodes) do
-		if ((node:getName() == 'autojumpstart') or (node:getName() == 'autojumpend')) then
-			local a = {}
-			a['element_name'] = node:getName()
-			for i,name in ipairs(attributes[node:getName()]) do
-				a[name] = node:getAttribute(name)
-			end
-			table.insert(autojumpNodes,a)
-		end
-	end
-
+	local autojumpNodes = getAutojumpNodesFromMap(mapFile)
 
 	-- copy data to autojump_data.xml and add it to map 
 	local newFile = XML.load(string.format(':%s/autojump_data.xml',mapName))
@@ -190,6 +211,21 @@ function copyAutojumpElementsToMap(mapFile,mapName,mapMeta)
 
 end
 
+function getAutojumpNodesFromMap (mapFile)
+	local autojumpNodes = {}
+	local mapNodes = mapFile:getChildren()
+	for i,node in ipairs(mapNodes) do
+		if ((node:getName() == 'autojumpstart') or (node:getName() == 'autojumpend')) then
+			local a = {}
+			a['element_name'] = node:getName()
+			for i,name in ipairs(attributes[node:getName()]) do
+				a[name] = node:getAttribute(name)
+			end
+			table.insert(autojumpNodes,a)
+		end
+	end
+	return autojumpNodes
+end
 
 -- this ivalidates save when you modify autojumps, to warn you if you don't save before testing
 function invalidate ()
